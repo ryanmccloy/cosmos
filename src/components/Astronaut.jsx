63c6fefe6136691d531c94bac5113103cedca04d
Ryan McCloy <ryanmccloy@Ryans-MacBook-Air.local>
2024-12-01 17:20:44 +0200

    satellite animation

--- src/components/Astronaut.jsx
+++ src/components/Astronaut.jsx
@@ -2,7 +2,7 @@ import Blob from "./Blob";
 
 function Astronaut() {
   return (
-    <div className="relative min-h-[406px] max-w-[400px]">
+    <div className="relative h-[406px] w-[406px]">
       {/* Astronaut animation is in index.css */}
       <img
         className="animate-astronaut absolute left-4 top-10 z-10 h-[406px] w-[288px] -rotate-[20deg]"
@@ -12,7 +12,7 @@ function Astronaut() {
       {/* Satellite animation is in index.css */}
       <img
         src="/assets/satellite.svg"
-        className="absolute left-64 top-24 z-10 h-[80px] w-[72px]"
+        className="animate-satellite absolute left-64 top-24 z-10 h-[80px] w-[72px]"
         alt="A satellite floating through space"
       />
       <Blob />
